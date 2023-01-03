429405e65a91282a1aee5686313b6c362d7f9f74
frank_chang <frank_chang@edisonedutech.com>
2023-01-03 17:42:50 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Linq;
+Console.WriteLine("Hello, World!");
+dynamic stuff = JObject.Parse(" {'0':'315','1':'320'}");
+
+Console.WriteLine(stuff);
+var n = stuff["0"];
+Console.WriteLine(n);
+var n2 = stuff["1"];
+Console.WriteLine(n2);
+
+//string name = stuff.0;
+//string address = stuff.Address.City;
